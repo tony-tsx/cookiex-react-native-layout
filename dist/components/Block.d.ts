d68eb8ee20c35338436c2be1b1579b958618181f
tony-tsx <tony.js@zoho.eu>
2020-02-27 14:25:16 -0300

    deletes

--- dist/components/Block.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ViewProps } from 'react-native';
-import Style from '../utils/Style';
-import Event from '../utils/Event';
-declare const Block: React.StatelessComponent<Block.Props>;
-declare namespace Block {
-    type Props = ViewProps & Style.Props & Event.On<Event.View>;
-}
-export default Block;
